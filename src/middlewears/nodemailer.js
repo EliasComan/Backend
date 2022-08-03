29a98cd07008dffd9dd043ede7f975b95bdc96de
EliasComan <comanelias5@gmail.com>
2022-08-03 11:35:22 -0300

    s

--- src/middlewears/nodemailer.js
+++ src/middlewears/nodemailer.js
@@ -1,8 +1,6 @@
 const {createTransport} = require('nodemailer')
 
 
-const TEST_EMAIL = 'alena88@ethereal.email';
-const PASS_EMAIL = '231w21aNfrntCQ6Yv8';
 const USER_GMAIL = 'comanelias5@gmail.com'
 const PASS_GMAIL ='puviydbskzmdxebo'
 module.exports.transporter =  createTransport({
